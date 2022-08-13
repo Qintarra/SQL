b2751fe9044f213bac2181bf0579e23435b48915
Qintarra <brain_damage@ukr.net>
2022-08-13 11:02:12 +0300

    file moved

---
+++ PostgreSQL/SELECT_Statement.sql
@@ -0,0 +1,45 @@
+-- DML: SELECT Statement
+
+SELECT * 
+FROM film;
+--
+
+SELECT title, release_year
+FROM film;
+ORDER BY title;
+--
+
+SELECT title, release_year
+FROM film;
+ORDER BY release_year DESC;
+--
+
+SELECT title, release_year
+FROM film;
+ORDER BY release_year, title;
+--
+
+SELECT title, release_year  
+FROM film 
+ORDER BY 2, 1;
+--
+
+SELECT title AS movie_title, release_year AS movie_release_year  
+FROM film 
+ORDER BY 1;
+--
+
+SELECT title AS movie_title, release_year AS movie_release_year  
+FROM film 
+ORDER BY movie_release_year DESC;
+--
+
+SELECT DISTINCT rating  
+FROM film 
+ORDER BY 1;
+--
+
+SELECT actor_id, first_name, last_name  
+FROM actor  
+ORDER BY 2, 3;
+--
