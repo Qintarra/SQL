b3a08f2d1ed38cbe656d9da71262af2057336a13
Qintarra <brain_damage@ukr.net>
2022-08-15 08:18:32 +0300

    having count

--- PostgreSQL/DML-SELECT_Statement.sql
+++ PostgreSQL/DML-SELECT_Statement.sql
@@ -138,3 +138,12 @@ ON fa.actor_id = a.actor_id
 GROUP BY a.first_name, a.last_name
 ORDER BY count_of_film;
 --
+
+SELECT a.first_name, a.last_name, COUNT(*) AS count_of_film
+FROM actor a
+INNER JOIN film_actor fa
+ON fa.actor_id = a.actor_id 
+GROUP BY a.first_name, a.last_name
+HAVING COUNT (*) > 40
+ORDER BY count_of_film DESC;
+--
