7de8bbc6461d7fdc84eefe514875cf44b1e6fb01
Qintarra <brain_damage@ukr.net>
2022-08-16 18:05:05 +0300

    insert

--- PostgreSQL/DML-INSERT_UPDATE_DELETE.sql
+++ PostgreSQL/DML-INSERT_UPDATE_DELETE.sql
@@ -8,3 +8,10 @@ SELECT * -- to check if city was added
 FROM city c
 WHERE c.country_id = 100;
 --
+
+INSERT INTO city (city, country_id)
+
+SELECT 'Kharkiv', cnt.country_id
+FROM country cnt
+WHERE UPPER (cnt.country) = 'UKRAINE';
+--
