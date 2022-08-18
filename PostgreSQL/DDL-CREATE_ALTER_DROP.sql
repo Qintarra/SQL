38772e7d6d0243b10284e42793a0cf9f01c7e839
Qintarra <brain_damage@ukr.net>
2022-08-18 13:08:38 +0300

    create table/ insert into

--- PostgreSQL/DDL-CREATE_ALTER_DROP.sql
+++ PostgreSQL/DDL-CREATE_ALTER_DROP.sql
@@ -19,3 +19,19 @@ VALUES (101);
 
 SELECT * FROM training_data.dim_groups; -- check the value
 --
+
+CREATE TABLE training_data.dim_trainees
+(
+	trainee_id		BIGSERIAL PRIMARY KEY,
+	group_id		BIGINT NOT NULL REFERENCES training_data.dim_groups,
+	first_name		TEXT NOT NULL,
+	last_name		TEXT NOT NULL,
+	full_name		TEXT GENERATED ALWAYS AS (first_name || ' ' || last_name) STORED NOT NULL, -- stroed on disc after being generated
+	birth_date		DATE NOT NULL,
+	enrollment_year INTEGER DEFAULT EXTRACT (year FROM current_date) NOT NULL,
+	graduation_year INTEGER
+);
+
+INSERT INTO training_data.dim_trainees(group_id, first_name, last_name, birth_date)
+VALUES (1, 'Rocky', 'Balboa', '1945-07-06'::DATE)
+--
