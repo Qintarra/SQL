dba5992d89d671aa222b21102d79eb64b7ca0d3b
Qintarra <brain_damage@ukr.net>
2022-08-18 13:07:22 +0300

    create table

--- PostgreSQL/DDL-CREATE_ALTER_DROP.sql
+++ PostgreSQL/DDL-CREATE_ALTER_DROP.sql
@@ -5,3 +5,17 @@ CREATE DATABASE training_center;
 
 CREATE SCHEMA training_data;
 --
+
+CREATE TABLE training_data.dim_groups
+(
+	group_id		BIGSERIAL PRIMARY KEY, -- constraint
+	group_number	INTEGER NOT NULL, -- natural key
+	creation_year	INTEGER DEFAULT EXTRACT (year FROM current_date),
+	disband_year	INTEGER
+);
+
+INSERT INTO training_data.dim_groups (group_number)
+VALUES (101);
+
+SELECT * FROM training_data.dim_groups; -- check the value
+--
