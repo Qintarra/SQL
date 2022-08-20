b064a52987b534cc379889eb65c546a3d73ccecc
Qintarra <brain_damage@ukr.net>
2022-08-20 13:33:09 +0300

    grant all

--- PostgreSQL/DCL-Statements.sql
+++ PostgreSQL/DCL-Statements.sql
@@ -2,3 +2,6 @@
 
 GRANT SELECT ON TABLE film TO dvd_rental_client; -- grants privilege to extract data from table film
 --
+
+GRANT ALL ON TABLE	film TO	dvd_rental_admin; -- grants all available privileges for this table. Not recommended. Dangerous, but aacceptable.  
+--
