8e9f6cb3880bd80e15454a47967d192a5621f215
Qintarra <brain_damage@ukr.net>
2022-08-24 11:24:20 +0300

    one more example

--- PostgreSQL/Ranking-Window-Functions.sql
+++ PostgreSQL/Ranking-Window-Functions.sql
@@ -26,3 +26,17 @@ GROUP BY ch.channel_desc,
 |Partners    |2000-10            |         7,508|5       |
 |Internet    |2000-10            |         1,450|6       |
 */
+
+SELECT	 ch.channel_desc,
+		 t.calendar_month_desc,
+		 TO_CHAR (TRUNC (SUM (amount_sold), -5), '9,999,999,999') AS sales$,
+		 ROW_NUMBER () OVER (PARTITION BY ch.channel_desc ORDER BY TRUNC (SUM (amount_sold), -5) DESC) AS row_number
+FROM 	 sh.sales s 
+ JOIN	 sh.products p ON p.prod_id = s.prod_id 
+ JOIN	 sh.customers cust ON cust.cust_id = s.cust_id 
+ JOIN 	 sh.times t ON t.time_id = s.time_id 
+ JOIN 	 sh.channels ch ON ch.channel_id = s.channel_id 
+ JOIN 	 sh.countries cn ON cn.country_id = cust.country_id
+WHERE	 t.calendar_month_desc IN ('2000-09', '2000-10')
+GROUP BY ch.channel_desc,
+		 calendar_month_desc;
