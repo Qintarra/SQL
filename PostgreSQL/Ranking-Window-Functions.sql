2f9ad202212d1632bd71bb92d41aed0043c50c14
Qintarra <brain_damage@ukr.net>
2022-08-25 16:08:18 +0300

    rank vs dense_rank

--- PostgreSQL/Ranking-Window-Functions.sql
+++ PostgreSQL/Ranking-Window-Functions.sql
@@ -80,3 +80,18 @@ GROUP BY ch.channel_desc,
 |Internet    |2000-10            |         1,450|6       |
 */
 --
+
+SELECT	 ch.channel_desc,
+		 t.calendar_month_desc,
+		 TO_CHAR (TRUNC (SUM (amount_sold), -5), '9,999,999,999') AS sales$,
+		 RANK () OVER (ORDER BY TRUNC (SUM (amount_sold), -5) DESC) AS rank, 
+		 DENSE_RANK () OVER (ORDER BY TRUNC (SUM (amount_sold), -5) DESC) AS dense_rank
+FROM 	 sh.sales s 
+ JOIN	 sh.products p ON p.prod_id = s.prod_id 
+ JOIN	 sh.customers cust ON cust.cust_id = s.cust_id 
+ JOIN 	 sh.times t ON t.time_id = s.time_id 
+ JOIN 	 sh.channels ch ON ch.channel_id = s.channel_id 
+ JOIN 	 sh.countries cn ON cn.country_id = cust.country_id
+WHERE	 t.calendar_month_desc IN ('2000-09', '2000-10')
+GROUP BY ch.channel_desc,
+		 calendar_month_desc;
