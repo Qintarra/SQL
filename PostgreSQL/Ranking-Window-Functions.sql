748be216ced2404c7b76c3f1f797a8fec159c705
Qintarra <brain_damage@ukr.net>
2022-08-27 12:55:05 +0300

    alignment added

--- PostgreSQL/Ranking-Window-Functions.sql
+++ PostgreSQL/Ranking-Window-Functions.sql
@@ -4,9 +4,9 @@
 
 --Display the sales amount by the channels for 09.2000 - 10.2000 and rank all rows of the data set by the sales amount
 SELECT	 ch.channel_desc,
-		 t.calendar_month_desc,
-		 TO_CHAR (TRUNC (SUM (amount_sold), -5), '9,999,999,999') AS sales$,
-		 ROW_NUMBER () OVER (ORDER BY TRUNC (SUM (amount_sold), -5) DESC) AS row_number -- the function TRUNC was applied to the sales amount to analyze how the function handles the same values 
+         t.calendar_month_desc,
+         TO_CHAR (TRUNC (SUM (amount_sold), -5), '9,999,999,999') AS sales$,
+         ROW_NUMBER () OVER (ORDER BY TRUNC (SUM (amount_sold), -5) DESC) AS row_number -- the function TRUNC was applied to the sales amount to analyze how the function handles the same values 
 FROM 	 sh.sales s 
  JOIN	 sh.products p ON p.prod_id = s.prod_id 
  JOIN	 sh.customers cust ON cust.cust_id = s.cust_id 
@@ -15,7 +15,7 @@ FROM 	 sh.sales s
  JOIN 	 sh.countries cn ON cn.country_id = cust.country_id
 WHERE	 t.calendar_month_desc IN ('2000-09', '2000-10')
 GROUP BY ch.channel_desc,
-		 calendar_month_desc;
+         calendar_month_desc;
 /*
 |channel_desc|calendar_month_desc|sales_count   |col_rank|
 |------------|-------------------|--------------|--------|
@@ -29,9 +29,9 @@ GROUP BY ch.channel_desc,
 
 -- Now rank the rows not whithin the entire data set, but within the partition by channels
 SELECT	 ch.channel_desc,
-		 t.calendar_month_desc,
-		 TO_CHAR (TRUNC (SUM (amount_sold), -5), '9,999,999,999') AS sales$,
-		 ROW_NUMBER () OVER (PARTITION BY ch.channel_desc ORDER BY TRUNC (SUM (amount_sold), -5) DESC) AS row_number
+         t.calendar_month_desc,
+         TO_CHAR (TRUNC (SUM (amount_sold), -5), '9,999,999,999') AS sales$,
+         ROW_NUMBER () OVER (PARTITION BY ch.channel_desc ORDER BY TRUNC (SUM (amount_sold), -5) DESC) AS row_number
 FROM 	 sh.sales s 
  JOIN	 sh.products p ON p.prod_id = s.prod_id 
  JOIN	 sh.customers cust ON cust.cust_id = s.cust_id 
@@ -40,7 +40,7 @@ FROM 	 sh.sales s
  JOIN 	 sh.countries cn ON cn.country_id = cust.country_id
 WHERE	 t.calendar_month_desc IN ('2000-09', '2000-10')
 GROUP BY ch.channel_desc,
-		 calendar_month_desc;
+         calendar_month_desc;
 /*
 |channel_desc|calendar_month_desc|sales$        |row_number|
 |------------|-------------------|--------------|----------|
@@ -57,9 +57,9 @@ GROUP BY ch.channel_desc,
 
 -- RANK on Multiple Expressions
 SELECT	 ch.channel_desc,
-		 t.calendar_month_desc,
-		 TO_CHAR (SUM (quantity_sold), '9,999,999,999') AS sales_count,
-		 RANK () OVER (ORDER BY calendar_month_desc, SUM (s.quantity_sold) DESC) AS col_rank
+         t.calendar_month_desc,
+         TO_CHAR (SUM (quantity_sold), '9,999,999,999') AS sales_count,
+         RANK () OVER (ORDER BY calendar_month_desc, SUM (s.quantity_sold) DESC) AS col_rank
 FROM 	 sh.sales s 
  JOIN	 sh.products p ON p.prod_id = s.prod_id 
  JOIN	 sh.customers cust ON cust.cust_id = s.cust_id 
@@ -68,7 +68,7 @@ FROM 	 sh.sales s
  JOIN 	 sh.countries cn ON cn.country_id = cust.country_id
 WHERE	 t.calendar_month_desc IN ('2000-09', '2000-10')
 GROUP BY ch.channel_desc,
-		 calendar_month_desc;
+         calendar_month_desc;
 /*
 |channel_desc|calendar_month_desc|sales_count   |col_rank|
 |------------|-------------------|--------------|--------|
@@ -83,10 +83,10 @@ GROUP BY ch.channel_desc,
 
 -- RANK vs DENSE_RANK
 SELECT	 ch.channel_desc,
-		 t.calendar_month_desc,
-		 TO_CHAR (TRUNC (SUM (amount_sold), -5), '9,999,999,999') AS sales$,
-		 RANK () OVER (ORDER BY TRUNC (SUM (amount_sold), -5) DESC) AS rank, 
-		 DENSE_RANK () OVER (ORDER BY TRUNC (SUM (amount_sold), -5) DESC) AS dense_rank
+         t.calendar_month_desc,
+         TO_CHAR (TRUNC (SUM (amount_sold), -5), '9,999,999,999') AS sales$,
+         RANK () OVER (ORDER BY TRUNC (SUM (amount_sold), -5) DESC) AS rank, 
+         DENSE_RANK () OVER (ORDER BY TRUNC (SUM (amount_sold), -5) DESC) AS dense_rank
 FROM 	 sh.sales s 
  JOIN	 sh.products p ON p.prod_id = s.prod_id 
  JOIN	 sh.customers cust ON cust.cust_id = s.cust_id 
@@ -95,7 +95,7 @@ FROM 	 sh.sales s
  JOIN 	 sh.countries cn ON cn.country_id = cust.country_id
 WHERE	 t.calendar_month_desc IN ('2000-09', '2000-10')
 GROUP BY ch.channel_desc,
-		 calendar_month_desc;
+         calendar_month_desc;
 /*
 |channel_desc|calendar_month_desc|sales$        |rank|dense_rank|
 |------------|-------------------|--------------|----|----------|
@@ -110,9 +110,9 @@ GROUP BY ch.channel_desc,
 
 -- RANK + PARTITION BY
 SELECT	 ch.channel_desc,
-		 t.calendar_month_desc,
-		 TO_CHAR (SUM (amount_sold), '9,999,999,999') AS sales$,
-		 RANK () OVER (PARTITION BY ch.channel_desc ORDER BY SUM (amount_sold) DESC) AS rank_by_channel
+         t.calendar_month_desc,
+         TO_CHAR (SUM (amount_sold), '9,999,999,999') AS sales$,
+         RANK () OVER (PARTITION BY ch.channel_desc ORDER BY SUM (amount_sold) DESC) AS rank_by_channel
 FROM 	 sh.sales s 
  JOIN	 sh.products p ON p.prod_id = s.prod_id 
  JOIN	 sh.customers cust ON cust.cust_id = s.cust_id 
@@ -122,7 +122,7 @@ FROM 	 sh.sales s
 WHERE	 t.calendar_month_desc IN ('2000-08', '2000-09', '2000-10', '2000-11')
 AND		 ch.channel_desc IN ('Direct Sales', 'Internet')
 GROUP BY ch.channel_desc,
-		 calendar_month_desc;
+         calendar_month_desc;
 /*
 |channel_desc|calendar_month_desc|sales$        |rank_by_channel|
 |------------|-------------------|--------------|---------------|
@@ -139,17 +139,17 @@ GROUP BY ch.channel_desc,
 
 -- Per Group Ranking
 SELECT	 ch.channel_desc,
-		 t.calendar_month_desc,
-		 TO_CHAR (SUM (amount_sold), '9,999,999,999') AS sales$,
-		 RANK () OVER (PARTITION BY calendar_month_desc ORDER BY SUM (amount_sold) DESC) AS rank_within_month,
-		 RANK () OVER (PARTITION BY ch.channel_desc ORDER BY SUM (amount_sold) DESC) AS rank_within_channel
+         t.calendar_month_desc,
+         TO_CHAR (SUM (amount_sold), '9,999,999,999') AS sales$,
+         RANK () OVER (PARTITION BY calendar_month_desc ORDER BY SUM (amount_sold) DESC) AS rank_within_month,
+         RANK () OVER (PARTITION BY ch.channel_desc ORDER BY SUM (amount_sold) DESC) AS rank_within_channel
 FROM 	 sh.sales s 
  JOIN 	 sh.times t ON t.time_id = s.time_id 
  JOIN 	 sh.channels ch ON ch.channel_id = s.channel_id 
 WHERE	 t.calendar_month_desc IN ('2000-08', '2000-09', '2000-10', '2000-11')
 AND		 ch.channel_desc IN ('Direct Sales', 'Internet')
 GROUP BY ch.channel_desc,
-		 calendar_month_desc
+         calendar_month_desc
 ORDER BY 1, 4, 5;
 /*
 |channel_desc|calendar_month_desc|sales$        |rank_within_month|rank_within_channel|
@@ -165,18 +165,18 @@ ORDER BY 1, 4, 5;
 */
 -- rewrite this example with an alternative syntax
 SELECT	 ch.channel_desc,
-		 t.calendar_month_desc,
-		 TO_CHAR (SUM (amount_sold), '9,999,999,999') AS sales$,
-		 RANK () OVER month_w AS rank_within_month,
-		 RANK () OVER channel_w AS rank_within_channel
+         t.calendar_month_desc,
+         TO_CHAR (SUM (amount_sold), '9,999,999,999') AS sales$,
+         RANK () OVER month_w AS rank_within_month,
+         RANK () OVER channel_w AS rank_within_channel
 FROM 	 sh.sales s 
  JOIN 	 sh.times t ON t.time_id = s.time_id 
  JOIN 	 sh.channels ch ON ch.channel_id = s.channel_id 
 WHERE	 t.calendar_month_desc IN ('2000-08', '2000-09', '2000-10', '2000-11')
 AND		 ch.channel_desc IN ('Direct Sales', 'Internet')
 GROUP BY ch.channel_desc,
-		 calendar_month_desc
+         calendar_month_desc
 WINDOW	 month_w AS (PARTITION BY calendar_month_desc ORDER BY SUM (amount_sold) DESC),
-		 channel_w AS (PARTITION BY ch.channel_desc ORDER BY SUM (amount_sold) DESC)
+         channel_w AS (PARTITION BY ch.channel_desc ORDER BY SUM (amount_sold) DESC)
 ORDER BY 1, 4, 5;
 --
