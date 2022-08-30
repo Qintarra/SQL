c2a748c370681fbc5ee6b17e48c1d61a6a2decf0
Qintarra <brain_damage@ukr.net>
2022-08-30 10:04:43 +0300

    table

--- PostgreSQL/Offset-Window-Functions.sql
+++ PostgreSQL/Offset-Window-Functions.sql
@@ -36,3 +36,22 @@ WHERE 	 calendar_month_number IN (1, 2, 3, 4)
 GROUP BY channel_desc,
          calendar_month_number
 ORDER BY 2;
+/*
+|channel_desc|calendar_month_number|sales$      |min_sales_month|
+|------------|---------------------|------------|---------------|
+|Internet    |1                    |679,299.05  |679,299.05     |
+|Partners    |1                    |2,092,590.24|679,299.05     |
+|Direct Sales|1                    |5,703,058.55|679,299.05     |
+|Internet    |2                    |1,018,813.31|1,018,813.31   |
+|Partners    |2                    |2,410,599.08|1,018,813.31   |
+|Direct Sales|2                    |5,522,651.25|1,018,813.31   |
+|Tele Sales  |3                    |53,395.37   |53,395.37      |
+|Internet    |3                    |1,069,167.19|53,395.37      |
+|Partners    |3                    |1,876,050.63|53,395.37      |
+|Direct Sales|3                    |4,680,793.47|53,395.37      |
+|Tele Sales  |4                    |42,712.02   |42,712.02      |
+|Internet    |4                    |981,296.96  |42,712.02      |
+|Partners    |4                    |2,091,816.42|42,712.02      |
+|Direct Sales|4                    |4,578,892.91|42,712.02      |
+*/
+--
