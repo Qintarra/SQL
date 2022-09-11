089b16c09e55045a2eda94244241acf02dd7130e
Qintarra <brain_damage@ukr.net>
2022-09-11 11:07:00 +0300

    insert into street

--- MySQL/02.Insert-data/insert.sql
+++ MySQL/02.Insert-data/insert.sql
@@ -1,3 +1,9 @@
 INSERT INTO city (id, name)
 VALUES  (1, 'Dnipro'), (2, 'Kyiv'), (3, 'Lviv');
 
+INSERT INTO street (id, name, city_id)
+VALUES  (1, 'Shevchenko', 1), 
+		(2, 'Volkova', 2), 
+		(3, 'Kioto', 2), 
+		(4, 'Garmatna', 2), 
+		(5, 'Naukova', 3);
