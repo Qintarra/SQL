72f1cb148ad872221234e3d09f161e0a6618a9bb
Qintarra <brain_damage@ukr.net>
2022-09-11 11:07:21 +0300

    insert into supermarket

--- MySQL/02.Insert-data/insert.sql
+++ MySQL/02.Insert-data/insert.sql
@@ -7,3 +7,12 @@ VALUES  (1, 'Shevchenko', 1),
 		(3, 'Kioto', 2), 
 		(4, 'Garmatna', 2), 
 		(5, 'Naukova', 3);
+
+INSERT INTO supermarket (id, name, street_id, house_number)
+VALUES  (1, 'ATB', 2, 15), 
+		(2, 'Auchan', 5, 24), 
+		(3, 'Fora', 1, 16), 
+		(4, 'Metro', 2, 5), 
+		(5, 'Novus', 4, 53),
+		(6, 'Silpo', 3, 63),
+		(7, 'Varus', 3, 21);
