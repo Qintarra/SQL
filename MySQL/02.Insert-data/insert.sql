2e6d7a86e8368eaeab20150402d8dd73cf23267b
Qintarra <brain_damage@ukr.net>
2022-09-12 18:20:44 +0300

    insert into person

--- MySQL/02.Insert-data/insert.sql
+++ MySQL/02.Insert-data/insert.sql
@@ -16,3 +16,15 @@ VALUES  (1, 'ATB', 2, 15),
 		(5, 'Novus', 4, 53),
 		(6, 'Silpo', 3, 63),
 		(7, 'Varus', 3, 21);
+
+INSERT INTO person (id, name, surname, birth_date)
+VALUES  (1, 'Yevgen', 'Kojevskiy', '2000-01-01'),
+		(2,	'Yan', 'Petrenko', '2001-11-01'),
+		(3,	'Janna', 'Ivanova', '2002-12-12'),
+		(4,	'Denis', 'Galushko', '1995-10-01'),
+		(5,	'Filip', 'Kromvel', '1980-08-08'),
+		(6,	'Bogdan', 'Steciuk', '2002-07-01'),
+		(7, 'Yeva', 'Krasivaya', '1992-08-08'),
+		(8,	'Nadegda', 'Vesela', '1997-05-11'),
+		(9,	'Arsen', 'Govorliviy', '1990-11-21'),
+		(10, 'Mariya', 'Velikolepnaya', '1988-04-01');
