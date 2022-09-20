fff114c8685d2470efde4d5bb96a851c9654ee97
Qintarra <brain_damage@ukr.net>
2022-09-20 11:22:02 +0300

    third task

--- MySQL/04.Aggregation/aggregation_task.sql
+++ MySQL/04.Aggregation/aggregation_task.sql
@@ -5,3 +5,8 @@ FROM product;
 -- Find the total number of supermarkets (supermarket_count).
 SELECT COUNT (id) AS supermarket_count
 FROM supermarket;
+
+-- Find the total number of customers (customer_count) with discount of more than 0.
+SELECT COUNT (person_id) AS customer_count
+FROM customer
+WHERE discount > 0;
