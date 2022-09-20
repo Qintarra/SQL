eddfd76288b0e400dee176088972a2effd050a20
Qintarra <brain_damage@ukr.net>
2022-09-20 11:21:44 +0300

    second task

--- MySQL/04.Aggregation/aggregation_task.sql
+++ MySQL/04.Aggregation/aggregation_task.sql
@@ -1,3 +1,7 @@
 ﻿-- Find the average price from the pricelist (price_average).
 SELECT AVG (price) AS price_average
 FROM product;
+
+-- Find the total number of supermarkets (supermarket_count).
+SELECT COUNT (id) AS supermarket_count
+FROM supermarket;
