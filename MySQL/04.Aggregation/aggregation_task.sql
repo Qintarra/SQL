27ea812895a00afba089a98dc5996bea24a40feb
Qintarra <brain_damage@ukr.net>
2022-09-20 11:22:18 +0300

    fourth task

--- MySQL/04.Aggregation/aggregation_task.sql
+++ MySQL/04.Aggregation/aggregation_task.sql
@@ -10,3 +10,10 @@ FROM supermarket;
 SELECT COUNT (person_id) AS customer_count
 FROM customer
 WHERE discount > 0;
+
+-- Find the number of products sold at a discount (product_total), sales revenue (to_pay_total) and discount amount (discount_total).
+SELECT  SUM (product_amount) AS product_total,
+		SUM (price_with_discount * product_amount) AS to_pay_total,
+		SUM ((price - price_with_discount)* product_amount) AS discount_total
+FROM    order_details
+WHERE  (price_with_discount < price);
