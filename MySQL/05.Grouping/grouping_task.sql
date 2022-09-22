ec0b09c66b7aae44dedae280144b331f7e7c1387
Qintarra <brain_damage@ukr.net>
2022-09-22 12:50:55 +0300

    1st task

---
+++ MySQL/05.Grouping/grouping_task.sql
@@ -0,0 +1,5 @@
+﻿-- Find the number of customers for each discount (customer_count) and their discount (discount), sorted ascending by discount.
+SELECT COUNT (person_id) AS customer_count, discount
+FROM customer
+GROUP BY discount
+ORDER BY discount;
