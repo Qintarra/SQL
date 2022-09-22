89709e694168bbd9940682a5583573fb10bd33c9
Qintarra <brain_damage@ukr.net>
2022-09-22 12:52:46 +0300

    all tasks completed

--- MySQL/05.Grouping/grouping_task.sql
+++ MySQL/05.Grouping/grouping_task.sql
@@ -11,3 +11,9 @@ GROUP BY customer_order_id
 HAVING SUM (price * product_amount) > 100
 ORDER BY customer_order_id;
 
+-- Find the order with the highest total discount value among all orders and get its discount value (max_order_discount).
+SELECT SUM((price - price_with_discount) * product_amount) AS max_order_discount
+FROM order_details
+GROUP BY customer_order_id
+ORDER BY max_order_discount DESC
+LIMIT 1;
