84f5d7512d06c39208245b63fadaf042faf39e6a
Qintarra <brain_damage@ukr.net>
2022-09-22 12:51:44 +0300

    group by + having

--- MySQL/05.Grouping/grouping_task.sql
+++ MySQL/05.Grouping/grouping_task.sql
@@ -3,3 +3,11 @@ SELECT COUNT (person_id) AS customer_count, discount
 FROM customer
 GROUP BY discount
 ORDER BY discount;
+
+-- Find for which orders (customer_order_id) the total price (total cost) of products (to_pay) is more than 100, sorted ascending by customer_order_id.
+SELECT customer_order_id, SUM (price * product_amount) AS to_pay
+FROM order_details
+GROUP BY customer_order_id
+HAVING SUM (price * product_amount) > 100
+ORDER BY customer_order_id;
+
