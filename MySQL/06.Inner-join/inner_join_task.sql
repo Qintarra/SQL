4ee6be93379c70dd832179ce9a0098e3ed6e0a77
Qintarra <brain_damage@ukr.net>
2022-09-23 11:25:57 +0300

    task 2

--- MySQL/06.Inner-join/inner_join_task.sql
+++ MySQL/06.Inner-join/inner_join_task.sql
@@ -3,3 +3,11 @@ SELECT product.id, product_title.title, product.price
 FROM product
 INNER JOIN product_title ON product.product_title_id = product_title.id
 ORDER BY title;
+
+-- Select products (id, title, category name, price), sorted ascending by category name and product title.
+SELECT product.id, product_title.title, product_category.name AS category, product.price
+FROM product
+INNER JOIN product_title ON product.product_title_id = product_title.id
+INNER JOIN product_category ON product_category_id = product_category.id
+ORDER BY category, title;
+
