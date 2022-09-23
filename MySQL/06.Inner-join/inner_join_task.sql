d8492df9000d7f93bbe1d25f72f7efbd9ab2b651
Qintarra <brain_damage@ukr.net>
2022-09-23 11:25:40 +0300

    task 1

---
+++ MySQL/06.Inner-join/inner_join_task.sql
@@ -0,0 +1,5 @@
+﻿-- Select products (id, title, price), sorted ascending by title.
+SELECT product.id, product_title.title, product.price
+FROM product
+INNER JOIN product_title ON product.product_title_id = product_title.id
+ORDER BY title;
