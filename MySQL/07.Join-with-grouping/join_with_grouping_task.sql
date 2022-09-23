4f393588c0ba3d0b7a94715f87d4f4228281933f
Qintarra <brain_damage@ukr.net>
2022-09-23 11:33:29 +0300

    1st query

---
+++ MySQL/07.Join-with-grouping/join_with_grouping_task.sql
@@ -0,0 +1,8 @@
+﻿-- Select non-empty categories with the total product number for each one (category name, count), sorted ascending by name.
+SELECT product_category.name AS category, COUNT (product.id) AS count
+FROM product_category
+INNER JOIN product_title ON product_category_id = product_category.id
+INNER JOIN product ON product_title_id = product_title.id
+WHERE category IS NOT NULL
+GROUP BY name
+ORDER BY name;
