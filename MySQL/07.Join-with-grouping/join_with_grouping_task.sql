0862fca3fab3f4cbebd42364fb38470210e2c6c8
Qintarra <brain_damage@ukr.net>
2022-09-24 09:17:40 +0300

    sales revenue

--- MySQL/07.Join-with-grouping/join_with_grouping_task.sql
+++ MySQL/07.Join-with-grouping/join_with_grouping_task.sql
@@ -6,3 +6,15 @@ INNER JOIN product ON product_title_id = product_title.id
 WHERE category IS NOT NULL
 GROUP BY name
 ORDER BY name;
+
+-- Select sales revenue for each city (city, income) on 1 Nov 2020 – 30 Nov 2020 inclusive (the date format is YYYY-MM-DD), sorted ascending by revenue and city.
+SELECT city.name AS city, SUM (order_details.price_with_discount * order_details.product_amount) AS income
+FROM order_details
+INNER JOIN customer_order ON customer_order_id = customer_order.id
+INNER JOIN supermarket ON supermarket_id = supermarket.id
+INNER JOIN street ON street_id = street.id
+INNER JOIN city ON city_id = city.id
+WHERE customer_order.operation_time BETWEEN '2020-11-01' AND '2020-11-30'
+GROUP BY city.name
+ORDER BY city, income;
+
