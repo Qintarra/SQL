98dee8699a311e4acfaac31446e89dd2ea4337da
Qintarra <brain_damage@ukr.net>
2022-09-26 09:39:34 +0300

    customers query

--- MySQL/08.Outer-join/outer_join_task.sql
+++ MySQL/08.Outer-join/outer_join_task.sql
@@ -7,3 +7,12 @@ INNER JOIN product_category ON product_category_id = product_category.id
 WHERE product_id IS NULL
 ORDER BY product_category.id;
 
+-- Get a list of authorized customers (last name, first name, date of birth) who has no orders in retailer stores. Sort ascending by last name, and birth date.
+SELECT surname, name, birth_date
+FROM person
+LEFT JOIN customer ON person_id = person.id
+LEFT JOIN customer_order ON customer_id = person_id
+LEFT JOIN order_details ON customer_order_id = customer_order.id
+WHERE customer_order_id IS NULL AND person_id = person.id
+ORDER BY 1, 3;
+
