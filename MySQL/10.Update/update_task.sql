984ef085e4aa0c11b85695b1705b6efcccd12c61
Qintarra <brain_damage@ukr.net>
2022-10-09 12:12:41 +0300

    discount on some products

--- MySQL/10.Update/update_task.sql
+++ MySQL/10.Update/update_task.sql
@@ -3,3 +3,12 @@ UPDATE supermarket
 SET name = 'Your Friend'
 WHERE name IN ('Your Choice', 'kramnichka');
 
+-- In the products table, all fruits and vegetables should fall in price by 25% of their current value.
+UPDATE product 
+SET price = price * 0.75
+WHERE id IN
+        (SELECT product.id
+        FROM product
+        INNER JOIN product_title ON product_title.id = product_title_id
+        INNER JOIN product_category ON product_category.id = product_category_id
+        WHERE product_category.id IN (1, 3));
