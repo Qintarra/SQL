0112e78bc34b135d6e121868cc179bc37f81c22c
Qintarra <brain_damage@ukr.net>
2022-10-09 12:12:00 +0300

    rename supermarkets

---
+++ MySQL/10.Update/update_task.sql
@@ -0,0 +1,5 @@
+﻿-- Rename supermarkets by replacing the old name "Your Choice" or "kramnichka" with the new "Your Friend".
+UPDATE supermarket
+SET name = 'Your Friend'
+WHERE name IN ('Your Choice', 'kramnichka');
+
