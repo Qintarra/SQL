1727b43def1a25e391f595d5df54dd46765465bf
Qintarra <brain_damage@ukr.net>
2022-10-10 10:36:20 +0300

    2nd query

--- MySQL/11.Delete/delete_task.sql
+++ MySQL/11.Delete/delete_task.sql
@@ -1,3 +1,7 @@
 ﻿-- From table "contact_type" delete record with type of "viber".
 DELETE FROM contact_type
 WHERE name = 'viber';
+
+-- From table "customer_order" delete orders 1, 7 and 10.
+DELETE FROM customer_order
+WHERE id IN (1, 7, 10);
