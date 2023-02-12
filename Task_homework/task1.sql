66c4213fd5c55440d297bf3f466a7df32cd635e0
qintarra <96946215+qintarra@users.noreply.github.com>
2023-02-12 10:42:23 +0200

    insert rows into employee_salary table

--- Task_homework/task1.sql
+++ Task_homework/task1.sql
@@ -106,3 +106,46 @@ create table employee_salary(
 		id serial primary key,
 		employee_id int not null unique,
 		salary_id int not null);
+
+-- 6. Insert 40 rows into the employee_salary table  
+insert into employee_salary(employee_id, salary_id)
+values (3, 7),
+		(4, 2),
+		(63, 11),
+		(32, 5),
+		(5, 3),
+		(42, 6),
+		(25, 8),
+		(14, 2),
+		(53, 4),
+		(45, 13), --10
+		(52, 5),
+		(38, 15),
+		(16,1),
+		(67, 3),
+		(60, 10),
+		(6, 11),
+		(50, 2),
+		(31, 5),
+		(40, 4),
+		(29, 8), --20
+		(24, 7),
+		(56, 7),
+		(47, 6),
+		(18, 9),
+		(15, 12),
+		(37, 7),
+		(26, 4),
+		(10, 14),
+		(21, 2),
+		(30, 9), --30
+		(71, 4),
+		(73, 12),
+		(75, 1),
+		(77, 4),
+		(79, 3),
+		(80, 3),
+		(85, 5),
+		(88, 8),
+		(91, 13),
+		(95, 7); --40
