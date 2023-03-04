a81d46de90b94ddcec84a6f02dfaf050565ca575
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-04 11:01:47 +0200

    all employees whose salary is less than 2000

--- Task_homework/task2.sql
+++ Task_homework/task2.sql
@@ -3,3 +3,10 @@ select employee_name, monthly_salary
 from employees 
 join employee_salary on employee_salary.employee_id = employees.id
 join salary on employee_salary.salary_id = salary.id;
+
+-- 2. All employees whose salary is less than 2000.  
+select employee_name, monthly_salary 
+from employees 
+join employee_salary on employee_salary.employee_id = employees.id
+join salary on employee_salary.salary_id = salary.id
+where monthly_salary < 2000;
