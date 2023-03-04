718ac96c0a730b8b3a1e187d5af6bde2fced9c6a
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-04 11:02:50 +0200

    All salary positions less than 2000 but no employee has been assigned to them

--- Task_homework/task2.sql
+++ Task_homework/task2.sql
@@ -16,3 +16,10 @@ select employee_name, monthly_salary
 from employees 
 right join employee_salary on employee_salary.employee_id = employees.id
 right join salary on employee_salary.salary_id = salary.id;
+
+-- 4. All salary positions less than 2000 but no employee has been assigned to them. (There is a salary, but it is not clear who receives it).  
+select employee_name, monthly_salary 
+from employees 
+right join employee_salary on employee_salary.employee_id = employees.id
+right join salary on employee_salary.salary_id = salary.id;
+where monthly_salary < 2000;
