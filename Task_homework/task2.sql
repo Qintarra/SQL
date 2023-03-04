8bbb08ece1748483dc197e639c9d90e0dc9dcbc7
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-04 11:02:22 +0200

    All salary positions, but no employee has been assigned to them

--- Task_homework/task2.sql
+++ Task_homework/task2.sql
@@ -10,3 +10,9 @@ from employees
 join employee_salary on employee_salary.employee_id = employees.id
 join salary on employee_salary.salary_id = salary.id
 where monthly_salary < 2000;
+
+-- 3. All salary positions, but no employee has been assigned to them. (There is a salary, but it is not clear who receives it).  
+select employee_name, monthly_salary 
+from employees 
+right join employee_salary on employee_salary.employee_id = employees.id
+right join salary on employee_salary.salary_id = salary.id;
