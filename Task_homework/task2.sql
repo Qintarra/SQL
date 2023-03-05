e2983965fecae2f09436c1b6aa86ffff05253206
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-06 00:16:58 +0200

    Names and salaries of middle specialists

--- Task_homework/task2.sql
+++ Task_homework/task2.sql
@@ -79,3 +79,12 @@ join roles on roles_employee.role_id = roles.id
 join employee_salary on employee_salary.employee_id = employees.id
 join salary on salary_id = salary.id
 where role_name like '%Jun%';
+
+-- 13. Names and salaries of middle specialists.  
+select employee_name, monthly_salary, role_name 
+from roles_employee 
+join employees on roles_employee.employee_id = employees.id
+join roles on roles_employee.role_id = roles.id 
+join employee_salary on employee_salary.employee_id = employees.id
+join salary on salary_id = salary.id
+where role_name like '%Mid%';
