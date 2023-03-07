25f55060bec1d81c30ca53dfa054c7fc33b84570
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-07 18:22:29 +0200

    Maximum salary of QA engineers

--- Task_homework/task2.sql
+++ Task_homework/task2.sql
@@ -175,3 +175,12 @@ join roles on roles_employee.role_id = roles.id
 join employee_salary on employee_salary.employee_id = employees.id
 join salary on salary_id = salary.id
 where role_name like '%QA%';
+ 
+-- 24. Maximum salary of QA engineers.  
+select max(monthly_salary) as max_qa_salary
+from roles_employee 
+join employees on roles_employee.employee_id = employees.id
+join roles on roles_employee.role_id = roles.id 
+join employee_salary on employee_salary.employee_id = employees.id
+join salary on salary_id = salary.id
+where role_name like '%QA%';
